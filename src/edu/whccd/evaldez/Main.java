0f47ad40dc24f2c04e931d94a57f694dd6e317f6
evandazar <eval2380@giant.cos.edu>
2020-03-17 21:59:07 -0700

    Initial Commit: Outputted Hello World into the console.

--- src/edu/whccd/evaldez/Main.java
+++ src/edu/whccd/evaldez/Main.java
@@ -1,9 +1,12 @@
 package edu.whccd.evaldez;
-
+/*
+ The purpose of this application is to output Hello World!
+ Author: Evander Valdez
+*/
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.print("Hello World!");
+        System.out.println("Hello World!");
     }
 }
